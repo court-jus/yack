15316ba48ab73ff2f5c2cbd264e0aff6ae452d67
Ghislain Lévêque <ghislain.leveque@gmail.com>
2015-01-06 23:10:41 +0100

    I can now open an image and crop it with input layout

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
-yack
-====
+YACK - Yet Another Cardmaking Kit
+=================================
 
 [![Gitter](https://badges.gitter.im/Join%20Chat.svg)](https://gitter.im/court-jus/yack?utm_source=badge&utm_medium=badge&utm_campaign=pr-badge&utm_content=badge)
 
-Yet Another Cardmaking Kit
-
 This tool allows one to open a PDF file containing play cards, split the file to extract the different cards and output another file with cards layed out differently
+
+Example:
+![Screen shot](https://github.com/court-jus/yack/raw/master/scrot/yack_teasing1.jpg "Teasing")
--- main.py
+++ main.py
@@ -39,35 +39,62 @@ INNW = px(10)
 INNH = px(10)
 
 class Yack(QtWidgets.QMainWindow, Ui_MainWindow):
-    def __init__(self):
+    def __init__(self, filename=None):
         super(Yack, self).__init__()
         self.setupUi(self)
         self._scene = QtWidgets.QGraphicsScene()
         self.graphicsView.setScene(self._scene)
-        with Image(filename='/tmp/micro.pdf', resolution=WORK_RESOLUTION) as img:
-            npages = len(img.sequence)
-            ncards = TILEW * TILEH
-            page = img.sequence[0]
-            rownum = 1
-            colnum = 1
-            left = MLEFT + colnum * (CARDW + INNW)
-            right = left + CARDW
-            top = MTOP + rownum * (CARDH + INNH)
-            bottom = top + CARDH
-            with page[left-30:right+30, top-30:bottom+30] as card:
-                xpm = card.make_blob(format='xpm')
-                pix = QtGui.QPixmap()
-                loaded = pix.loadFromData(xpm)
-                if loaded:
-                    print("Loaded")
-                self._scene.addPixmap(pix)
+        self.actionExit.triggered.connect(self.close)
+        self.inputCardWidth.setValue(CARDW)
+        self.inputCardHeight.setValue(CARDH)
+        self.inputRows.setValue(TILEH)
+        self.inputColumns.setValue(TILEW)
+        self.inputOuterLeft.setValue(MLEFT)
+        self.inputOuterTop.setValue(MTOP)
+        self.inputInnerWidth.setValue(INNW)
+        self.inputInnerHeight.setValue(INNH)
+        self.actionOpen.triggered.connect(self.chooseFile)
+        if filename:
+            self.openFile(filename)
+
+    def chooseFile(self):
+        filename, truc = QtWidgets.QFileDialog.getOpenFileName(self, "Open file...", None,
+            "PDF (*.pdf);;All files (*)")
+        if filename:
+            self.openFile(filename)
+
+    def openFile(self, filename):
+        with Image(filename=filename, resolution=WORK_RESOLUTION) as img:
+            xpm = img.make_blob(format='xpm')
+            pix = QtGui.QPixmap()
+            loaded = pix.loadFromData(xpm)
+            if loaded:
+                print("Loaded")
+            self._scene.addPixmap(pix)
+#            npages = len(img.sequence)
+#            page = img.sequence[0]
+#            rownum = 1
+#            colnum = 1
+#            left = self.inputOuterLeft.value() + colnum * (self.inputCardWidth.value() + self.inputInnerWidth.value())
+#            right = left + self.inputCardWidth.value()
+#            top = self.inputOuterTop.value() + rownum * (self.inputCardHeight.value() + self.inputInnerHeight.value())
+#            bottom = top + self.inputCardHeight.value()
+#            print("left",left,"right",right,"top",top,"bottom",bottom)
+#            print("imgsize", img.size)
+#            with page[left:right, top:bottom] as card:
+#                xpm = card.make_blob(format='xpm')
+#                pix = QtGui.QPixmap()
+#                loaded = pix.loadFromData(xpm)
+#                if loaded:
+#                    print("Loaded")
+#                self._scene.addPixmap(pix)
 
 
 if __name__ == '__main__':
 
     app = QtWidgets.QApplication(sys.argv)
 
-    yack = Yack()
+    yack = Yack(filename=sys.argv[1] if len(sys.argv) > 0 else None)
     yack.show()
 
     sys.exit(app.exec_())
