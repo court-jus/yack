2703ff4a0cc2965299e8917f9ed60b3d2c996ff9
Ghislain Lévêque <ghislain.leveque@gmail.com>
2015-01-09 23:00:17 +0100

    Now we have 3 views to show input, current card and output (not implemented yet) and we can load/save layouts

--- main.py
+++ main.py
@@ -2,8 +2,10 @@
 
 import sys
 import pdb
+import traceback
 import os
 import time
+import json
 
 # QT
 from PyQt5.QtCore import Qt
@@ -15,69 +17,58 @@ from wand.image import Image
 # YACK
 from ui.main_ui import Ui_MainWindow
 
-WORK_RESOLUTION = 300
-BASEDIR = '/tmp'
-# MM = INCH / 25.4
-
-def unit_convert(unit=1.0, base_resolution=300, work_resolution=300):
-
-    def px(val):
-        if work_resolution != base_resolution:
-            return int(val * unit * float(work_resolution) / float(base_resolution))
-        return int(val * unit)
-
-    return px
-
-px = unit_convert(work_resolution=WORK_RESOLUTION)
-    
-CARDW = px(750)
-CARDH = px(1050)
-TILEW = 3
-TILEH = 3
-INNW = px(0)
-INNH = px(0)
+WORK_RESOLUTION = 50
 
 class Yack(QtWidgets.QMainWindow, Ui_MainWindow):
     def __init__(self, filename=None):
         super(Yack, self).__init__()
         self.setupUi(self)
-        self._scene = QtWidgets.QGraphicsScene()
+        self.inputScene = QtWidgets.QGraphicsScene()
+        self.cardScene = QtWidgets.QGraphicsScene()
+        self.outputScene = QtWidgets.QGraphicsScene()
         self._image = None
         self._image_cache = {}
         self._center = [0, 0]
         self.currentPage = 0
         self.pageCount = 1
         self.currentCard = 4
-        self.graphicsView.setScene(self._scene)
+        self.inputGView.setScene(self.inputScene)
+        self.cardGView.setScene(self.cardScene)
+        self.cardGView.scale(3, 3)
+        self.outputGView.setScene(self.outputScene)
+        # Menu
         self.actionExit.triggered.connect(self.close)
-        self.inputCardWidth.setValue(CARDW)
-        self.inputCardHeight.setValue(CARDH)
-        self.inputRows.setValue(TILEH)
-        self.inputColumns.setValue(TILEW)
-        self.shiftHor.setValue(0)
-        self.shiftVert.setValue(0)
-        self.inputInnerWidth.setValue(INNW)
-        self.inputInnerHeight.setValue(INNH)
         self.actionOpen.triggered.connect(self.chooseFile)
         self.actionSaveCards.triggered.connect(self.exportCards)
-        self.rotateButton.clicked.connect(self.fn1)
+        # Top buttons
+        self.rotateButton.clicked.connect(self.rotate)
         self.zoomInButton.clicked.connect(lambda: self.zoom(1.5))
         self.zoomOutButton.clicked.connect(lambda: self.zoom(1.0/1.5))
         self.prevPageButton.clicked.connect(lambda: self.setCurrent(relPage=-1))
         self.nextPageButton.clicked.connect(lambda: self.setCurrent(relPage=1))
         self.prevCardButton.clicked.connect(lambda: self.setCurrent(relCard=-1))
         self.nextCardButton.clicked.connect(lambda: self.setCurrent(relCard=1))
+        # Input buttons
+        self.openInputLayoutButton.clicked.connect(lambda: self.openLayout())
+        self.saveInputLayoutButton.clicked.connect(lambda: self.saveLayout())
+        # Output buttons
+        self.copyLayoutButton.clicked.connect(
+            lambda: self.dictToLayout(self.layoutToDict(), 'output'))
+        self.openOutputLayoutButton.clicked.connect(lambda: self.openLayout('output'))
+        self.saveOutputLayoutButton.clicked.connect(lambda: self.saveLayout('output'))
         for widget in [
             self.inputCardWidth, self.inputCardHeight,
             self.inputRows, self.inputColumns,
-            self.shiftHor, self.shiftVert,
+            self.inputShiftHor, self.inputShiftVert,
             self.inputInnerWidth, self.inputInnerHeight,
         ]:
+            if WORK_RESOLUTION != 300:
+                widget.setValue(int(widget.value() * WORK_RESOLUTION / 300))
             widget.valueChanged.connect(self.updateCard)
-        self.showFullPageCB.toggled.connect(self.toggleFullPage)
         if filename:
             self.openFile(filename)
 
+    # Choose and update current
     def setCurrent(self, page=None, card=None, relPage=None, relCard=None):
         cp = self.currentPage
         cc = self.currentCard
@@ -94,58 +85,43 @@ class Yack(QtWidgets.QMainWindow, Ui_MainWindow):
             "{0}/{1} - {2}/{3}".format(self.currentPage + 1, self.pageCount,
                                        self.currentCard + 1, cardCount)
         )
+        if self.currentPage != cp:
+            self.showPixmap(
+                self.inputScene, 'page{0}'.format(self.currentPage),
+                self.showFullPage, page=self.currentPage)
         if self.currentPage != cp or self.currentCard != cc:
-            self.updateCard()
+            self.showPixmap(
+                self.cardScene,
+                'page{0}card{1}'.format(self.currentPage, self.currentCard),
+                self.showCard, page=self.currentPage, card=self.currentCard)
 
-    def toggleFullPage(self):
-        if self._image is None:
-            return
-        if self.showFullPageCB.isChecked():
-            self.showPixmap('page{0}'.format(self.currentPage), self.showFullPage,
-                            page=self.currentPage)
-        else:
-            self.showPixmap('page{0}card{1}'.format(self.currentPage, self.currentCard),
-                            self.showCard, page=self.currentPage, card=self.currentCard)
+    def updateAll(self):
+        self.showPixmap(
+            self.inputScene, 'page{0}'.format(self.currentPage),
+            self.showFullPage, page=self.currentPage, force=True)
+        self.showPixmap(
+            self.cardScene,
+            'page{0}card{1}'.format(self.currentPage, self.currentCard),
+            self.showCard, page=self.currentPage, card=self.currentCard,
+            force=True)
 
     def updateCard(self):
-        self.showPixmap('page{0}card{1}'.format(self.currentPage, self.currentCard),
+        self.showPixmap(self.cardScene, 'page{0}card{1}'.format(self.currentPage, self.currentCard),
                         self.showCard, page=self.currentPage, card=self.currentCard,
                         force=True)
 
-    def fn1(self):
-        self.graphicsView.rotate(90)
-
-    def zoom(self, factor):
-        self.graphicsView.scale(factor, factor)
-
-    def chooseFile(self):
-        filename, truc = QtWidgets.QFileDialog.getOpenFileName(self, "Open file...", None,
-            "PDF (*.pdf);;All files (*)")
-        if filename:
-            self.openFile(filename)
-
-    def openFile(self, filename):
-        self._image_cache = {}
-        with Image(filename=filename, resolution=WORK_RESOLUTION) as img:
-            self.pageCount = len(img.sequence)
-            self._center = [s/2 for s in img.size]
-            print(self._center)
-            self._image = img.make_blob()
-            self.setCurrent(page=0, card=0)
-            self.toggleFullPage()
-
-    def clearScene(self):
-        for i in self._scene.items():
-            self._scene.removeItem(i)
+    def clearScene(self, scene):
+        for i in scene.items():
+            scene.removeItem(i)
 
     def getPixmap(self, ident, fn, force=False, *args, **kwargs):
         if ident not in self._image_cache or force:
             self._image_cache[ident] = fn(*args, **kwargs)
         return self._image_cache[ident]
 
-    def showPixmap(self, *args, **kwargs):
-        self.clearScene()
-        self._scene.addPixmap(self.getPixmap(*args, **kwargs))
+    def showPixmap(self, scene, *args, **kwargs):
+        self.clearScene(scene)
+        scene.addPixmap(self.getPixmap(*args, **kwargs))
 
     def showFullPage(self, page=0):
         with Image(blob=self._image, resolution=WORK_RESOLUTION) as img:
@@ -155,32 +131,37 @@ class Yack(QtWidgets.QMainWindow, Ui_MainWindow):
             pix.loadFromData(xpm)
             return pix
 
-    def getCropCoords(self, card=0):
-        rownum = card // self.inputColumns.value()
-        colnum = card % self.inputColumns.value()
-        # TODO : handle even number of rows or columns
-        middle_row = self.inputColumns.value() // 2
-        middle_col = self.inputRows.value() // 2
-        left = (
-            self._center[0] - self.shiftHor.value() +
-            (colnum - middle_col) * self.inputCardWidth.value() -
-            self.inputCardWidth.value() / 2 +
-            self.inputInnerWidth.value() / 2
-        )
-        top = (
-            self._center[1] - self.shiftVert.value() +
-            (rownum - middle_row) * self.inputCardHeight.value() -
-            self.inputCardHeight.value() / 2 +
-            self.inputInnerHeight.value() / 2
-        )
-        width = self.inputCardWidth.value() - self.inputInnerWidth.value()
-        height = self.inputCardHeight.value() - self.inputInnerHeight.value()
-        return map(int, [left, top, width, height])
-
     def showCard(self, page=0, card=0):
         fullpage = self.getPixmap('page{0}'.format(page), self.showFullPage, page=page)
         left, top, width, height = self.getCropCoords(card)
         return fullpage.copy(left, top, width, height)
+
+    # Manipulate the view
+    def rotate(self):
+        self.inputGView.rotate(90)
+        self.cardGView.rotate(90)
+        self.outputGView.rotate(90)
+
+    def zoom(self, factor):
+        self.inputGView.scale(factor, factor)
+        self.cardGView.scale(factor, factor)
+        self.outputGView.scale(factor, factor)
+
+    # Manipulate files
+    def chooseFile(self):
+        filename, _ = QtWidgets.QFileDialog.getOpenFileName(self, "Open file...", None,
+            "PDF (*.pdf);;All files (*)")
+        if filename:
+            self.openFile(filename)
+
+    def openFile(self, filename):
+        self._image_cache = {}
+        with Image(filename=filename, resolution=WORK_RESOLUTION) as img:
+            self.pageCount = len(img.sequence)
+            self._center = [s/2 for s in img.size]
+            self._image = img.make_blob()
+            self.setCurrent(page=0, card=0)
+            self.updateAll()
 
     def exportCards(self):
         pages = range(self.pageCount)
@@ -199,6 +180,73 @@ class Yack(QtWidgets.QMainWindow, Ui_MainWindow):
                     with page[left:left+width, top:top+height] as card:
                         card.save(filename='page{0}card{1}.png'.format(p, c))
         self.statusbar.showMessage("Export done.", 1500)
+
+    def openLayout(self, layout='input'):
+        filename, _ = QtWidgets.QFileDialog.getOpenFileName(self, "Open layout file...", None,
+            "JSON (*.json);;All files (*)")
+        if filename:
+            try:
+                with open(filename, 'r') as fh:
+                    data = json.load(fh)
+                    self.dictToLayout(data, layout)
+            except ValueError:
+                traceback.print_exc()
+                self.statusbar.showMessage("Error loading or decoding layout file.")
+
+    def saveLayout(self, layout='input'):
+        filename, _ = QtWidgets.QFileDialog.getSaveFileName(self, "Save layout file...", None,
+            "JSON (*.json);;All files (*)")
+        if filename:
+            try:
+                with open(filename, 'w') as fh:
+                    json.dump(self.layoutToDict(layout), fh)
+            except ValueError:
+                traceback.print_exc()
+                self.statusbar.showMessage("Error loading or decoding layout file.")
+
+    # Helpers
+    def layoutToDict(self, layout='input'):
+        result = {}
+        for attrname in dir(self):
+            if attrname.startswith(layout):
+                widget = getattr(self, attrname)
+                key = attrname[len(layout):]
+                if hasattr(widget, 'value'):
+                    result[key] = widget.value()
+                elif hasattr(widget, 'text'):
+                    result[key] = widget.text()
+        return result
+
+    def dictToLayout(self, layoutDict, layout='input'):
+        for k, v in layoutDict.items():
+            if hasattr(self, layout + k):
+                widget = getattr(self, layout + k)
+                if hasattr(widget, 'setValue'):
+                    widget.setValue(int(v))
+                elif hasattr(widget, 'setText'):
+                    widget.setText(v)
+
+    def getCropCoords(self, card=0):
+        rownum = card // self.inputColumns.value()
+        colnum = card % self.inputColumns.value()
+        # TODO : handle even number of rows or columns
+        middle_row = self.inputColumns.value() // 2
+        middle_col = self.inputRows.value() // 2
+        left = (
+            self._center[0] - self.inputShiftHor.value() +
+            (colnum - middle_col) * self.inputCardWidth.value() -
+            self.inputCardWidth.value() / 2 +
+            self.inputInnerWidth.value() / 2
+        )
+        top = (
+            self._center[1] - self.inputShiftVert.value() +
+            (rownum - middle_row) * self.inputCardHeight.value() -
+            self.inputCardHeight.value() / 2 +
+            self.inputInnerHeight.value() / 2
+        )
+        width = self.inputCardWidth.value() - self.inputInnerWidth.value()
+        height = self.inputCardHeight.value() - self.inputInnerHeight.value()
+        return map(int, [left, top, width, height])
 
 
 if __name__ == '__main__':
